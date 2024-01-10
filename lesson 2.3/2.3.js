280fac6dda12f04b9682a1ec2d65c35f4f3ae1d1
Mysnikan <154927225+Mysnikan@users.noreply.github.com>
2024-01-10 09:41:51 +0300

    2.3_lesson

---
+++ lesson 2.3/2.3.js
@@ -0,0 +1,6 @@
+let titleProject = 'Калькулятор по разработке сайта'
+let screensValue = 'шаблонные, с уникальным дизайном, с анимациями'
+let screenPrice = 20000
+let percentage = 6
+let fullPrice = 150000
+let responsive = true
